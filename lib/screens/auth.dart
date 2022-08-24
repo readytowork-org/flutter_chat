709bbdd5b34548b9c1bbd4cce50c84a1f75208ce
bantawa04 <rahunn3@gmail.com>
2022-08-24 17:32:03 +0545

    14.15 Firebase auth config

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import '../widgets/auth_form.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
 
@@ -8,9 +11,41 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
-  void _submitAuthForm(String email, String username, String password, bool isLogin){
+  final _auth = FirebaseAuth.instance;
 
+  void _submitAuthForm(
+    String email,
+    String username,
+    String password,
+    bool isLogin,
+  ) async {
+    UserCredential _authResult;
+    try {
+      if (isLogin) {
+        _authResult = await _auth.signInWithEmailAndPassword(
+            email: email, password: password);
+      } else {
+        _authResult = await _auth.createUserWithEmailAndPassword(
+            email: email, password: password);
+      }
+    } on PlatformException catch (error) {
+      var message = "An error occured, please check your credentials !";
+
+      if (error.message != null) {
+        message = error.message!;
+      }
+
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("Item added to your cart."),
+          duration: Duration(seconds: 2),
+        ),
+      );
+    } catch (err) {
+      print(err);
+    }
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
